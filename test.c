a462e7dc321e0ed25efd8ad0e5331ec39012c900
haosimaqi <1046408496@qq.com>
2019-11-25 00:11:48 -0800

    11111111

--- test.c
+++ test.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int main(int argc, const char *argv[])
 {
-	printf("hello ");
-	printf("hello--------- ");
+	printf("hello \n");
+	printf("hello--------- \n");
+	printf("cccccccc\n"):
 	return 0;
 }
